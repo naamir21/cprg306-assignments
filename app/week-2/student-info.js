c5e694f27fc4e050d83614e1887c35a43da2da9d
naamir21 <nida.aamir@edu.sait.ca>
2024-01-30 22:47:28 -0700

    Initial commit

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return (
+      <main>
+        <p>Nida Aamir</p>
+        <Link href="https://github.com">https://github.com</Link>
+      </main>
+    );
+  }
