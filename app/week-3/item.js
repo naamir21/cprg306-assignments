f5a04310a10522ebb617928405785d47f290b061
naamir21 <nida.aamir@edu.sait.ca>
2024-02-06 23:23:52 -0700

    Complete Week 3 Assignment

---
+++ app/week-3/item.js
@@ -0,0 +1,16 @@
+export default function Item({name, quantity, category}) {
+    return (
+        <div>
+            <ul className="hover:text-blue-400 p-2 animate-bounce">
+                <li>
+                    <h2 className="text-xl font-semibold">{name}</h2>
+                </li>
+                <li>
+                    <p className="text-sm ml-1">Buy {quantity} in {category}</p>
+                </li>
+            </ul>
+        </div>
+    )
+} 
+
+
